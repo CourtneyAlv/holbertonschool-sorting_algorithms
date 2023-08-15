74792760c380e89b73c8670461984e8cd315a053
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-14 17:51:49 -0700

    fixed 2-selection_sort.c file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,14 +14,14 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = j;
+		min_idx = i;
 
 	for (j = i + 1; j < size; j++)
 	{
 		if (array[j] < array[min_idx])
 		min_idx = j;
 	}
-	if (min_idx != 1)
+	if (min_idx != i)
 	{
 		int temp = array[i];
 	
