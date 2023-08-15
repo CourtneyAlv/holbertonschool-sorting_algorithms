f1e6cb3343895f54156ffca2e768f00c1457f946
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-14 17:02:59 -0700

    made some changes on 0-bubble_sort.c file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * @list: double pointer to the firat element of the list
+ * Return: Nothing
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *sort, *unsort, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+
+		sort = *list;
+		unsort = (*list)->next;
+
+	while (unsort != NULL)
+	{
+		while (sort != NULL && sort->n > unsort->n)
+		{
+			temp = unsort->next;
+
+		if (temp != NULL)
+		temp->prev = unsort->prev;
+
+		unsort->prev = sort->prev;
+		unsort->next = sort;
+
+		if (sort->prev != NULL)
+		sort->prev->next = unsort;
+
+		else
+		*list = unsort;
+		sort->prev = unsort;
+		sort = unsort->prev;
+
+		}
+		sort = unsort;
+		unsort = unsort->next;
+
+		print_list(*list);
+	}
+}
